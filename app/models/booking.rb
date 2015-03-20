3acd35c982afef5a8a2fbd54d8434376279f7144
Raja Hafify <rajahafify.zaed@gmail.com>
2015-03-20 16:13:59 +0800

    Adds validation for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -13,4 +13,7 @@
 #
 
 class Booking < ActiveRecord::Base
+  validates :email, presence: true
+  validates :start_date, presence: true 
+  validates :end_date, presence: true
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -17,10 +17,27 @@ require 'rails_helper'
 RSpec.describe Booking, type: :model do
   subject(:booking) { build :booking }
 
-  it { respond_to :email }
-  it { respond_to :name }
-  it { respond_to :contact_no }
-  it { respond_to :start_date }
-  it { respond_to :end_date }
-  it { be_valid }
+  it { is_expected.to respond_to :email }
+  it { is_expected.to respond_to :name }
+  it { is_expected.to respond_to :contact_no }
+  it { is_expected.to respond_to :start_date }
+  it { is_expected.to respond_to :end_date }
+  it { is_expected.to be_valid }
+
+  describe "validations" do
+    it "requires email" do
+      subject.email = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "requires start_date" do
+      subject.start_date = nil
+      expect(subject).not_to be_valid
+    end
+
+    it "requires end_date" do
+      subject.end_date = nil
+      expect(subject).not_to be_valid
+    end
+  end
 end
