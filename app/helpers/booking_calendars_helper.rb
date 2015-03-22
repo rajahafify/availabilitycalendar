4c6576a8b467ab36cd43b2935de0ceedefd97a82
Raja Hafify <rajahafify.zaed@gmail.com>
2015-03-22 14:27:35 +0800

    Fix calendaer button

--- app/helpers/booking_calendars_helper.rb
+++ app/helpers/booking_calendars_helper.rb
@@ -1,13 +1,24 @@
 module BookingCalendarsHelper
   def previous_link
-    ->(param, date_range) { link_to raw("&laquo; Prev"), {param => date_range.first - 1.day}, class: 'btn btn-default col-xs-3' }
+    link :previous
   end
 
   def next_link
-    ->(param, date_range) { link_to raw("Next &raquo;"), {param => date_range.last + 1.day}, class: 'btn btn-default col-xs-3' }
+    link :next
   end
 
   def title
     ->(start_date) { content_tag :h2, "#{I18n.t("date.month_names")[start_date.month]} #{start_date.year}", class: "calendar-title col-xs-6" }
+  end
+
+  private
+  
+
+  def link direction
+    if direction == :previous
+      ->(param, date_range) { link_to raw("&laquo; Prev"), root_path(start_date: date_range.first - 1.day), class: 'btn btn-default col-xs-3' }
+    else
+      ->(param, date_range) { link_to raw("Next &raquo;"), {param => date_range.last + 1.day}, class: 'btn btn-default col-xs-3' }
+    end
   end
 end
