96a59306420b14d0dbb2fae29de81135dddea844
Raja Hafify <rajahafify.zaed@gmail.com>
2015-03-22 13:17:56 +0800

    Adds styles for calendar header

--- app/helpers/booking_calendars_helper.rb
+++ app/helpers/booking_calendars_helper.rb
@@ -1,9 +1,13 @@
 module BookingCalendarsHelper
   def previous_link
-    ->(param, date_range) { link_to raw("&laquo;"), {param => date_range.first - 1.day}, class: 'btn btn-success' }
+    ->(param, date_range) { link_to raw("&laquo; Prev"), {param => date_range.first - 1.day}, class: 'btn btn-default col-xs-3' }
   end
 
   def next_link
-    ->(param, date_range) { link_to raw("&raquo;"), {param => date_range.last + 1.day}, class: 'btn btn-success' }
+    ->(param, date_range) { link_to raw("Next &raquo;"), {param => date_range.last + 1.day}, class: 'btn btn-default col-xs-3' }
+  end
+
+  def title
+    ->(start_date) { content_tag :h2, "#{I18n.t("date.month_names")[start_date.month]} #{start_date.year}", class: "calendar-title col-xs-6" }
   end
 end
